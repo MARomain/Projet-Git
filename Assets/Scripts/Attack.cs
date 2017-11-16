fded6c989d760d33e9b0587a1feec2c46e9e0b10
DBelbeze <belbeze.dorian@gmail.com>
2017-11-16 16:40:46 +0100

    attackWarriorAndMage

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    public int Damage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.GetComponent<Human>().takeDamage(Damage);
+        }
+    }
+}
